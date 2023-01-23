96831332187bd5d05f56ac878132915ded83232c
Alpha Olomi <alphaolomi@gmail.com>
2023-01-23 18:29:53 +0300

    setup program

---
+++ app/Models/Program.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Program extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    // public function registerMediaCollections(): void
+    // {
+    //     $this->addMediaCollection('documents');
+    // }
+}
