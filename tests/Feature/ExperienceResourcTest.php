85eb82a6f982f463ef42e100541ce3c815d4259f
Alpha Olomi <alphaolomi@gmail.com>
2023-03-13 12:33:32 +0300

    wip ExperienceResourcTest

---
+++ tests/Feature/ExperienceResourcTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use function Pest\Livewire\livewire;
+use App\Filament\Resources\ExperienceResource;
+use App\Filament\Resources;
+use App\Models\Experience;
+
+it('can render experience page', function () {
+    $this->get(ExperienceResource::getUrl('index'))->assertSuccessful();
+});
+
+it('can list posts', function () {
+    $Experiences = Experience::factory()->count(10)->create();
+
+
+    /** @var \Livewire\Testing\TestableLivewire $livewire */
+    $livewire = livewire(Resources\ExperienceResource\Pages\ListExperiences::class);
+
+    $livewire
+        ->assertSee($Experiences->first()->title)
+        ->assertSuccessful();
+    // ->assertCanSeeTableRecords($Experiences);
+});
